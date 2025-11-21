dca92a1937f7d23fe9440cd956dc2a50ef32c43e
RafiiFirdaus <muhammad.firdaus23@student.uisi.ac.id>
2025-11-21 20:06:15 +0700

    buat penyesuaian pada main.dart untuk dihubungkan ke loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:KirofTix/data/presentation/login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'KirofTix',
+      theme: ThemeData(
+        // Warna Utama (Dark Blue-Grey)
+        primaryColor: const Color(0xFF2C3E50),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2C3E50),
+          primary: const Color(0xFF2C3E50), // Warna tombol & appbar
+          secondary: const Color(0xFFE74C3C), // Warna aksen tombol logout & cancel
         ),
+        useMaterial3: true,
+        // default style input form
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          filled: true,
+          fillColor: Colors.grey[100],
+        )
       ),
+      home: const LoginPage(),
     );
   }
 }
